9cbab38d02194c4c09113caa2384386fd9d46d94
ivojawer <ivojawerbaum@gmail.com>
2019-04-02 17:01:51 -0300

    agrego todosAPartirDe

--- ejemplosRecursivos.hs
+++ ejemplosRecursivos.hs
@@ -1,6 +1,24 @@
+--Sin listas
+--todosAPartirDe :: Int -> [Int]
+
+todosAPartirDe numeroBase tope 
+    | tope >= numeroBase = [numeroBase] ++ todosAPartirDe (numeroBase+1) tope
+    | otherwise = []
+
+
+
+
+
+--Con listas
 
 foldl' fx seed [x] = fx seed x
 foldl' fx seed (x:xs) =foldl' fx (fx seed x) xs 
 
 sum' [x] = x
 sum' (x:xs) = x + sum' xs
+
+
+
+
+{-ordenar criterio [x] listaOrdenada = 
+ordenar criterio (x:xs) listaOrdenada = -}
